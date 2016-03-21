4a4b129f881309b4b81d23d323e9d92b2c180bae
Sergei Garcia Ortega <Sergei Garcia Ortega>
2016-03-21 14:33:49 -0600

    fixed bugs introduced on last push

--- src/components/behaviour/fill-height-directive.js
+++ src/components/behaviour/fill-height-directive.js
@@ -3,8 +3,8 @@ module.exports = fillHeight;
 function fillHeight() {
     return {
         restrict: 'A',
-        link: function(scope, el) {
-            var height = Math.max(window.innerHeight, document.documentElement.clientHeight || 0);
+        link: function(scope, el, attrs) {
+            var height = Number(attrs.percentage) || Math.max(window.innerHeight, document.documentElement.clientHeight || 0);
             el.css('height', height + 'px');
         }
     }
