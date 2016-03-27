960ca843dc71829afb7ce62f212ce7a32bd16f8c
Sergei Garcia Ortega <Sergei Garcia Ortega>
2016-03-27 17:20:12 -0600

    added chat input functionality

--- src/components/chat/chat-directive.js
+++ src/components/chat/chat-directive.js
@@ -4,15 +4,36 @@ function chat(eventFactory) {
         restrict: 'AE',
         controller: function() {
             var vm = this;
+            vm.chatLog = [];
             vm.input = '';
+            eventFactory.listen('chat-window', function(data) {
+                if (data.action === 'user-input') {
+                    vm.chatLog.push({
+                        author: 'user',
+                        message: vm.input,
+                        timestamp: new Date().toISOString()
+                    });
+                    vm.input = '';
+                    data.callback();
+                }
+            });
         },
         controllerAs: 'chatCtrl',
         link: function(scope, el) {
+            var chatInput = document.querySelector('.chat-input');
+            angular.element(chatInput).on('keyup', function(event) {
+                if (event.which === 13) {
+                    eventFactory.dispatch('chat-window', { action: 'user-input', callback: function() {
+                        el.scope().$apply();
+                    }});
+                }
+            });
             eventFactory.listen('chat-window', function(data) {
                 switch(data.action) {
                     case 'open':
                         if (!el.hasClass('is-active'))
                             el.addClass('is-active');
+                        chatInput.focus();
                         break;
                     case 'close':
                         if (el.hasClass('is-active'))
@@ -20,6 +41,9 @@ function chat(eventFactory) {
                         break;
                     case 'toggle':
                         el.toggleClass('is-active');
+                        if (el.hasClass('is-active')) {
+                            chatInput.focus();
+                        }
                         break;
                 }
             });
